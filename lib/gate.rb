89e13ee932cd3fa3b5d708d97c4c315482b9d126
residenti <n.queue.r@gmail.com>
2021-02-20 17:19:04 +0900

    7章 トライアルで書いたコードを参考コードを元に改良

--- lib/gate.rb
+++ lib/gate.rb
@@ -1,14 +1,28 @@
 class Gate
+  ROUTE = [:umeda, :juso, :mikuni]
+
   def initialize(name)
     @name = name
   end
 
   def enter(ticket)
-    ticket.enter_gate_name = @name
+    ticket.stamp(@name)
   end
 
   def exit(ticket)
-    ticket.exit_gate_name = @name
-    ticket.check
+    required_fare = calc_fare(ticket)
+    ticket.fare >= required_fare
+  end
+
+  private
+
+  def calc_fare(ticket)
+    from = ROUTE.index(ticket.stamp_at)
+    to = ROUTE.index(@name)
+    via_stations = ROUTE[(from + 1)...to]
+
+    return Ticket::BASE_FARE if via_stations.empty?
+
+    Ticket::BASE_FARE + (Ticket::ADDITIONAL_FARE * via_stations.count)
   end
 end
--- lib/ticket.rb
+++ lib/ticket.rb
@@ -1,23 +1,14 @@
 class Ticket
+  attr_reader :stamp_at, :fare
+
   BASE_FARE = 150
   ADDITIONAL_FARE = 40
-  ROUTE = [:umeda, :juso, :mikuni]
-
-  attr_accessor :enter_gate_name, :exit_gate_name
 
   def initialize(fare)
     @fare = fare
   end
 
-  def check
-    enter_gate_index = ROUTE.find_index(enter_gate_name)
-    exit_gate_index = ROUTE.find_index(exit_gate_name)
-    via_station_count = ROUTE[(enter_gate_index + 1)...exit_gate_index].count
-
-    if via_station_count.zero?
-      @fare >= BASE_FARE
-    else
-      @fare >= BASE_FARE + (ADDITIONAL_FARE * via_station_count)
-    end
+  def stamp(name)
+    @stamp_at = name
   end
 end
